b5a07603afeedb03525ec0384b195eefeda8029a
1029278668@qq.com <1029278668@qq.com>
2017-09-13 20:38:10 +0800

    init

--- src/views/nav2/Page2.vue
+++ src/views/nav2/Page2.vue
@@ -566,25 +566,7 @@
                     description: '',
                     equipmentCategory: [],
                     equipmentId: []
-                },
-                gridData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }]
-
+                }
             }
         },
         methods: {
